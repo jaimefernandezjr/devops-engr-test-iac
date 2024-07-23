78b2a07e32f0f8d90792a6da61a314d4f191ad75
jaimefernandezjr <jaimefernandez551@gmail.com>
2024-07-24 07:33:32 +0800

    add github and terraform scripts

---
+++ main.tf
@@ -0,0 +1,57 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+variable "public_key" {
+  description = "Public key for SSH access"
+  type        = string
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = var.public_key
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
+
+  security_groups = [aws_security_group.allow_http.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              service docker start
+              usermod -a -G docker ec2-user
+              docker run -d -p 3000:3000 --name rest-api-service YOUR_DOCKERHUB_USERNAME/rest-api-service:latest
+              EOF
+
+  tags = {
+    Name = "REST API Service"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.app.public_ip
+}
