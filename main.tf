9befc653a915a9982c5e7448c27b23e0d3eb1040
Jaime Jr Fernandez <jaimefernandez551@gmail.com>
2024-07-24 15:21:09 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-southeast-1"  # Ensure this is the correct region for your resources
+  region = "ap-southeast-1"
 }
 
 data "aws_vpc" "default" {
@@ -11,13 +11,13 @@ variable "public_ssh_key" {
   type        = string
 }
 
-data "aws_key_pair" "existing_key_pair" {
+data "aws_key_pair" "existing_keypair" {
   key_name = "deployer-key"
 }
 
 resource "aws_key_pair" "deployer" {
-  count     = length(data.aws_key_pair.existing_key_pair.id) == 0 ? 1 : 0
-  key_name  = "deployer-key"
+  count      = data.aws_key_pair.existing_keypair.key_name == "" ? 1 : 0
+  key_name   = "deployer-key"
   public_key = var.public_ssh_key
 }
 
@@ -26,11 +26,10 @@ data "aws_security_group" "existing_sg" {
     name   = "group-name"
     values = ["allow_http5"]
   }
-  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group" "allow_http5" {
-  count       = length(data.aws_security_group.existing_sg.id) == 0 ? 1 : 0
+  count       = length(data.aws_security_group.existing_sg.ids) == 0 ? 1 : 0
   name        = "allow_http5"
   description = "Allow HTTP inbound traffic"
   vpc_id      = data.aws_vpc.default.id
@@ -53,9 +52,9 @@ resource "aws_security_group" "allow_http5" {
 resource "aws_instance" "app" {
   ami           = "ami-012c2e8e24e2ae21d"  # Verify the AMI ID for your region
   instance_type = "t2.micro"
-  key_name      = length(data.aws_key_pair.existing_key_pair.id) == 0 ? aws_key_pair.deployer[0].key_name : data.aws_key_pair.existing_key_pair.key_name
+  key_name      = coalesce(data.aws_key_pair.existing_keypair.key_name, aws_key_pair.deployer[0].key_name)
 
-  vpc_security_group_ids = length(data.aws_security_group.existing_sg.id) == 0 ? [aws_security_group.allow_http5[0].id] : [data.aws_security_group.existing_sg.id]
+  vpc_security_group_ids = [coalesce(data.aws_security_group.existing_sg.id, aws_security_group.allow_http5[0].id)]
 
   user_data = <<-EOF
               #!/bin/bash
